ec7d8fda7f66c73e95e99c594acdda77c0652e1b
HOME <kraynoff.i@yandex.ru>
2022-12-16 21:23:53 +0300

    fixes

--- PythonSchool/notmy.py
+++ PythonSchool/notmy.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class Figure:
 
     def __init__(self, one, two, three):
@@ -16,15 +18,37 @@ class Triangle(Figure):
         return (polu_per * (polu_per - self.one) * (polu_per - self.two) * (polu_per - self.three))**(1 / 2)
 
 
-URL = "https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv"
-import pandas as pd
-df = pd.read_csv(URL)
+
+df = pd.read_csv('D:/WORKS/Python-Tests/PythonSchool/wild_data.csv')
+
+def size_f(size: str):
+    if size == 'Varies with device': return 0.
+    elif size.find('M') >= 0: return float(size.replace('M', ''))*1024
+    elif size.find('K') >= 0: return float(size.replace('K', ''))
+
+def tr_Content_Rating(rating):
+    rating =rating.replace('Everyone 10+','10').replace('Everyone','0').replace('Teen','16').replace('Mature 17+','17').replace('Adults only 18+', '18').replace('Unrated','0')
+
+    if rating == 'NaN': return 0
+    return int(rating)
+
+df['Size'] = df['Size'].apply(size_f)
+df['Installs'] = df['Installs'].apply(
+    lambda x: int(x.replace(',', '').replace('+', ''))
+    )
+df['Last Updated'] = pd.to_datetime(df['Last Updated'])
+df['Price'] = df['Price'].apply(lambda x: float(x.replace('$', '')))
+df['Content Rating'] = df['Content Rating'].apply(tr_Content_Rating)
+df.loc[df['Rating'].isna()]=0
+
+df4 = df.loc[df['Rating']>4.9]
+df5 =df.loc[df['Category']=='GAME'].loc[(df['Rating']>4.9) &(df['Price']==0)].drop(columns=['Price','Category'])
 
 print('Cleared:')
-df2 = df.drop(columns=['Sex','Pclass','Siblings/Spouses Aboard','Parents/Children Aboard','Fare'])
-print(df2)
-print('Age >18:')
-print(df2.loc[df['Age'] > 18])
+print(df)
 
-print('Survived')
-print(df2.loc[(df2['Age'] > 18) & (df['Survived']==1)].drop(columns=['Survived']))
+print('Rating >4.9 :')
+print(df4)
+
+print('Rating >4.9 Games')
+print(df5)
